666bd3f09af8f84e669878fe89200bcd1dd0c0e5
MatheusSparrowBR <matheus.deoliverasilva@gmail.com>
2026-09-24 00:07:38 -0300

    fix: forçar atualização do service worker após melhorias de Push

--- src/pwa-register.js
+++ src/pwa-register.js
@@ -1,4 +1,4 @@
-const SW_URL = '/sw.js?version=delivery-feedback-v5'
+const SW_URL = '/sw.js?version=delivery-feedback-v6'
 const UPDATE_INTERVAL_MS = 15 * 60 * 1000
 
 async function activateRegistrationUpdate(registration) {
